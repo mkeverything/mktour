b9138019f934f8aa5025e13a3d4b0c0735084098
Anatoly <yatskovanatoly@gmail.com>
2026-02-26 06:26:08 +0300

    chore: minor styling

--- src/components/ui-custom/input-ghost.tsx
+++ src/components/ui-custom/input-ghost.tsx
@@ -16,7 +16,7 @@ function InputGhost({
       type={type}
       data-slot="input"
       className={cn(
-        'file:text-foreground placeholder:text-primary selection:bg-primary selection:text-primary-foreground dark:bg-background max-w-full min-w-0 shrink rounded-md bg-transparent text-ellipsis whitespace-pre shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed',
+        'file:text-foreground placeholder:text-primary selection:bg-primary selection:text-primary-foreground dark:bg-background focus-visible:placeholder:text-muted-foreground max-w-full min-w-0 shrink rounded-md bg-transparent text-ellipsis whitespace-pre shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed',
         'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
         className,
       )}
