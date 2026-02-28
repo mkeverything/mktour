8a149e8d69d84c7c885d5ea11c1bf3d3dca8f646
sukalov <matvey10@gmail.com>
2026-02-28 22:59:13 +0300

    fix: lichess api endpoint teams/of/{username} has become protected

--- src/lib/api/lichess.ts
+++ src/lib/api/lichess.ts
@@ -1,10 +1,14 @@
 import { Team } from '@/types/lichess-api';
+import { cookies } from 'next/headers';
 
 export const getUserLichessTeams = async (
   username: string,
 ): Promise<Team[]> => {
   try {
-    const res = await fetch(`https://lichess.org/api/team/of/${username}`);
+    const token = (await cookies()).get('token')?.value;
+    const res = await fetch(`https://lichess.org/api/team/of/${username}`, {
+      headers: token ? { Authorization: `Bearer ${token}` } : {},
+    });
     if (!res.ok) {
       throw new Error(`Failed to fetch teams: ${res.statusText}`);
     }
