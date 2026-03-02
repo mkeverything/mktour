1c278087fe5f6b8603377d3e486abfdbdc6487ff
sukalov <matvey10@gmail.com>
2026-03-02 18:01:58 +0300

    Revert "chore: countdown till release time"
    This reverts commit a17683449c6d38e664779bdf2f0a30381b70837e.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import Loading from '@/app/loading';
-import Countdown from '@/components/countdown';
 import Navigation from '@/components/navigation';
 import ErrorFallback from '@/components/providers/error-boundary';
 import IntlProvider from '@/components/providers/intl-provider';
@@ -52,9 +51,7 @@ export default function RootLayout({ children }: PropsWithChildren) {
             <MediaQueryProvider>
               <TRPCReactProvider>
                 <Suspense fallback={<Loading />}>
-                  <Countdown>
-                    <LayoutContent>{children}</LayoutContent>
-                  </Countdown>
+                  <LayoutContent>{children}</LayoutContent>
                 </Suspense>
               </TRPCReactProvider>
             </MediaQueryProvider>
