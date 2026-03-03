892f3b2ee3dc4ce5c254d59d75f8440de3bb1fc5
sukalov <matvey10@gmail.com>
2026-03-03 13:59:14 +0300

    impr: remove empty club from public view

--- src/server/queries/get-all-clubs.ts
+++ src/server/queries/get-all-clubs.ts
@@ -1,17 +1,23 @@
 import { db } from '@/server/db';
 import { clubs } from '@/server/db/schema/clubs';
+import { tournaments } from '@/server/db/schema/tournaments';
 import { ClubModel } from '@/server/zod/clubs';
-import { desc } from 'drizzle-orm';
+import { desc, eq, isNotNull } from 'drizzle-orm';
 
 export default async function getAllClubs() {
-  // page: number = 1,
-  // pageSize: number = 10
   const allClubs = await db
-    .select()
+    .selectDistinct({
+      id: clubs.id,
+      name: clubs.name,
+      description: clubs.description,
+      createdAt: clubs.createdAt,
+      lichessTeam: clubs.lichessTeam,
+      allowPlayersSetResults: clubs.allowPlayersSetResults,
+    })
     .from(clubs)
+    .innerJoin(tournaments, eq(clubs.id, tournaments.clubId))
+    .where(isNotNull(tournaments.closedAt))
     .orderBy(desc(clubs.createdAt))
-    // .limit(pageSize)
-    // .offset((page - 1) * pageSize)
     .$dynamic();
   return allClubs as ClubModel[];
 }
