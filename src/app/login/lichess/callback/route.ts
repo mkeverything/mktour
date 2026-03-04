f9ea4895b2dc628fd0d6963ac08bca94812661a9
Anatoly <yatskovanatoly@gmail.com>
2026-03-04 04:53:18 +0300

    ref: minor

--- src/app/login/lichess/callback/route.ts
+++ src/app/login/lichess/callback/route.ts
@@ -85,7 +85,7 @@ export async function GET(request: Request): Promise<Response> {
       const userId = newid();
       const clubId = newid();
       const ctuId = `${clubId}=${userId}`;
-      const name = lichessUser.profile?.realName ?? undefined;
+      const name = lichessUser.profile?.realName;
 
       await db.insert(clubs).values({
         id: clubId,
