b01ace203e3051daef3b2b4a4fdf1d9636906135
Anatoly <yatskovanatoly@gmail.com>
2026-03-10 22:41:29 +0300

    ref: fallback skeleton list in paginator

--- src/components/ui-custom/paginator.tsx
+++ src/components/ui-custom/paginator.tsx
@@ -1,11 +1,12 @@
 import useOnReach from '@/components/hooks/use-on-reach';
-import { Skeleton } from '@/components/ui/skeleton';
-import { FC } from 'react';
+import SkeletonList from '@/components/skeleton-list';
+import { FC, ReactElement } from 'react';
 
 const Paginator: FC<Props> = ({
   hasNextPage,
   isFetchingNextPage,
   fetchNextPage,
+  skeleton = <SkeletonList length={3} />,
 }) => {
   const triggerRef = useOnReach(() => {
     if (hasNextPage && !isFetchingNextPage) {
@@ -19,13 +20,7 @@ const Paginator: FC<Props> = ({
         ref={triggerRef}
         className="h-0 w-full -translate-y-[calc(var(--spacing-mk-card-height)+calc((var(--spacing-mk)*2)))]"
       />
-      {isFetchingNextPage && (
-        <div className="flex flex-col gap-2">
-          {[1, 2, 3].map((i) => (
-            <Skeleton key={i} className="h-14 rounded-xl" />
-          ))}
-        </div>
-      )}
+      {isFetchingNextPage && skeleton}
     </div>
   );
 };
@@ -34,6 +29,7 @@ type Props = {
   hasNextPage: boolean;
   isFetchingNextPage: boolean;
   fetchNextPage: () => void;
+  skeleton?: ReactElement<typeof SkeletonList>;
 };
 
 export default Paginator;
