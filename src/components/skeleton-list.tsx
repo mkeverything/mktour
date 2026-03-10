d0c43345d9c47bf95406cf4f78cef9181ee78763
Anatoly <yatskovanatoly@gmail.com>
2026-03-10 23:24:47 +0300

    feat: bg card attribute for skeletonList

--- src/components/skeleton-list.tsx
+++ src/components/skeleton-list.tsx
@@ -5,12 +5,14 @@ import { FC } from 'react';
 const SkeletonList: FC<{
   length?: number;
   className?: string;
-}> = ({ length, className }) => {
+  card?: boolean;
+}> = ({ length, className, card }) => {
   const list = new Array(length || 5).fill('');
+  const from = card ? 'from-card' : 'from-background';
 
   return (
     <div className="mk-list relative z-10 h-full max-h-[60dvh] overflow-hidden">
-      <div className="from-background absolute inset-0 w-full bg-linear-to-t" />
+      <div className={`${from} absolute inset-0 w-full bg-linear-to-t`} />
       <div className="-z-10 flex flex-col gap-2">
         {list.map((_, i) => (
           <Skeleton key={i} className={cn(`h-18 w-full`, className)} />
