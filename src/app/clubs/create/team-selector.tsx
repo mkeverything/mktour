c96acba19747bdb69de93055462fab4af76f0f00
sukalov <matvey10@gmail.com>
2026-06-03 16:39:27 -0600

    ref: use proper controlled input inside team-selector

--- src/app/clubs/create/team-selector.tsx
+++ src/app/clubs/create/team-selector.tsx
@@ -19,80 +19,76 @@ import {
 } from '@/components/ui/select';
 import { X } from 'lucide-react';
 import { useTranslations } from 'next-intl';
-import { useState } from 'react';
 
 export function TeamSelector({ teams, form }: TeamSelectorProps) {
-  const [state, setState] = useState<boolean>(false);
   const t = useTranslations('Club.New');
 
   return (
     <FormField
       control={form.control}
       name="lichessTeam"
-      key={Number(state)}
-      render={({ field }) => (
-        <FormItem>
-          <FormLabel className="sr-only">{t('connect lichess team')}</FormLabel>
-          <Select
-            onValueChange={field.onChange}
-            defaultValue={field.value ?? undefined}
-            disabled={teams.length === 0}
-          >
-            <FormControl>
-              <SelectTrigger className="m-0">
-                <SelectValue
-                  placeholder={
-                    teams.length
-                      ? t('connect lichess team')
-                      : t('no lichess teams')
-                  }
-                >
+      render={({ field }) => {
+        const placeholder = teams.length
+          ? t('connect lichess team')
+          : t('no lichess teams');
+
+        return (
+          <FormItem>
+            <FormLabel className="sr-only">
+              {t('connect lichess team')}
+            </FormLabel>
+            <Select
+              onValueChange={field.onChange}
+              value={field.value ?? ''}
+              disabled={teams.length === 0}
+            >
+              <FormControl>
+                <SelectTrigger className="m-0">
+                  <div className="flex flex-row items-center">
+                    {field.value && <LichessLogo className="mr-2 size-3" />}
+                    <SelectValue placeholder={placeholder} />
+                  </div>
+                </SelectTrigger>
+              </FormControl>
+              {teams.length > 0 && (
+                <SelectContent>
+                  {teams.map((team) => (
+                    <SelectItem
+                      key={team.value}
+                      value={team.value}
+                      className={`${field.value && 'text-muted-foreground'}`}
+                      onClick={(e) => e.stopPropagation()}
+                    >
+                      {team.label}
+                    </SelectItem>
+                  ))}
                   {field.value && (
-                    <LichessLogo className="mr-2 inline size-3" />
+                    <Button
+                      id="removeSelection"
+                      className="h-[30px] w-full justify-start pl-8"
+                      variant="ghost"
+                      onClick={() => {
+                        form.setValue('lichessTeam', null, {
+                          shouldDirty: true,
+                          shouldTouch: true,
+                          shouldValidate: true,
+                        });
+                      }}
+                      style={{ pointerEvents: 'auto' }}
+                    >
+                      <X className="pr-2" />
+                      <span className="text-bold">
+                        {t('unselect lichess team')}
+                      </span>
+                    </Button>
                   )}
-                  {teams.find((team) => team.value === field.value)?.label}
-                </SelectValue>
-              </SelectTrigger>
-            </FormControl>
-            {teams.length > 0 && (
-              <SelectContent>
-                {teams.map((team) => (
-                  <SelectItem
-                    key={team.value}
-                    value={team.value}
-                    className={`${form.getValues('lichessTeam') && 'text-muted-foreground'}`}
-                    onClick={(e) => e.stopPropagation()}
-                  >
-                    {team.label}
-                  </SelectItem>
-                ))}
-                {form.getValues('lichessTeam') && (
-                  <Button
-                    id="removeSelection"
-                    className="h-[30px] w-full justify-start pl-8"
-                    variant="ghost"
-                    onClick={() => {
-                      form.setValue('lichessTeam', null, {
-                        shouldDirty: true,
-                        shouldTouch: true,
-                        shouldValidate: true,
-                      });
-                      setState(!state);
-                    }}
-                    style={{ pointerEvents: 'auto' }}
-                  >
-                    <X className="pr-2" />
-                    <span className="text-bold">
-                      {t('unselect lichess team')}
-                    </span>
-                  </Button>
-                )}
-              </SelectContent>
-            )}
-          </Select>
-          <FormMessage />
-        </FormItem>
-      )}
+                </SelectContent>
+              )}
+            </Select>
+            <FormMessage />
+          </FormItem>
+        );
+      }}
     />
   );
 }
