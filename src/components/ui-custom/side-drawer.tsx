9fbd040913bcfb5bef905ec71b1e4bb17c08f2c2
sukalov <matvey10@gmail.com>
2026-06-24 15:22:55 +0300

    fix: gap in side-drawer

--- src/components/ui-custom/side-drawer.tsx
+++ src/components/ui-custom/side-drawer.tsx
@@ -30,11 +30,11 @@ const SideDrawer: FC<DrawerProps> = ({
         <Drawer.Overlay className="fixed inset-0 top-0 z-50 bg-black/60 backdrop-blur-[calc(var(--blur-xs)/3)]" />
         <Drawer.Content
           onInteractOutside={() => setOpen(false)}
-          className="bg-background border-l-muted fixed top-0 right-0 bottom-0 z-50 h-dvh max-h-dvh w-auto gap-3 rounded-l-3xl border-0 border-l-[.5px] px-4 pt-[calc(1.5rem+env(safe-area-inset-top))] pb-[calc(1.5rem+env(safe-area-inset-bottom))] max-sm:left-[5rem] sm:w-sm"
+          className="bg-background border-l-muted fixed top-0 right-0 bottom-0 z-50 h-dvh max-h-dvh w-auto rounded-l-3xl border-0 border-l-[.5px] px-4 pt-[calc(1.5rem+env(safe-area-inset-top))] pb-[calc(1.5rem+env(safe-area-inset-bottom))] max-sm:left-[5rem] sm:w-sm"
         >
           <Drawer.Title hidden />
           <Drawer.Description hidden />
-          <div className="flex h-full min-h-0 w-full min-w-0 flex-1 flex-col">
+          <div className="flex h-full min-h-0 w-full min-w-0 flex-1 flex-col gap-3">
             {children}
           </div>
         </Drawer.Content>
